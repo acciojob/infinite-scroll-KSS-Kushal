8de88010b4d5bea617cecd44993abf853d6e7ced
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-17 20:21:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
 
+const container = document.getElementById("infi-list");
+let count = 11;
+for (let i = 1; i <= 10; i++) {
+	const list = document.createElement("li");
+	list.textContent = `Item ${i}`;
+	container.appendChild(list);
+}
+container.onscroll = () => {
+	 if ( 
+        Math.ceil(container.clientHeight  
+            + container.scrollTop) >= 
+            container.scrollHeight 
+    ) {
+		 const list = document.createElement("li");
+	list.textContent = `Item ${count}`;
+	container.appendChild(list);
+	list.textContent = `Item ${count++}`;
+	container.appendChild(list);
+	}
+}
